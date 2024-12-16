cddec6050634078c02e55e7ddde5874ab01e22b8
francescomerighi1202 <francescomerighi61@gmail.com>
2024-12-17 00:28:31 +0100

    Bug fix

--- components/maintenance/maintenance.jsx
+++ components/maintenance/maintenance.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 import styles from "./maintenance.module.css";
 
-import partialLogoBlack from "@/assets/logo-partiaL-black.svg";
+import cmgLogo from "@/assets/logo-partial-black.svg";
 
 export default function Maintenance() {
   return (
     <section className={styles.maintenance}>
-      <Image src={partialLogoBlack} alt="Logo" />
+      <Image src={cmgLogo} alt="Logo" />
       <h1>In arrivo...</h1>
     </section>
   );
